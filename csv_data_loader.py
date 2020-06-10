8b47aab3bc67e4b39e1ffbfbe76f194c80b05d1e
Fotone <jaemuon5582@gmail.com>
2020-06-10 18:02:47 +0900

    csv data loader

--- csv_data_loader.py
+++ csv_data_loader.py
@@ -1,9 +1,6 @@
 import json
-import numpy as np
-import random
-import matplotlib.pyplot as plt
 import pandas as pd
-from my_utils.note_embedding import note_map
+
 
 CSV_PATH = "./note_csv/"
 
@@ -17,35 +14,45 @@ def get_song_list():
 
 if __name__ == "__main__":
     song_list = get_song_list()["processed_list"]
-    id2note, note2id = note_map(4)
 
-    song = song_list[0]
-    title = song["title"]
-    diff_list = song["diff_list"]
+    beat_mean = 373.6436695278145
+    beat_std = 99.71674024965917
 
-    count_list = []
+    count_min = 0.0
+    count_max = 5.075173815233827
+
+    train_x = []
+    train_y = []
 
     for song in song_list:
         title = song["title"]
         diff_list = song["diff_list"]
+        beat = (song['beat'] - beat_mean) / beat_std
+
         for diff in diff_list:
+            count_list = []
+            note_list = []
+
+            difficulty_rating = float(diff["difficulty_rating"]) / 10
+            diff_overall = float(diff["diff_overall"]) / 10
+            diff_drain = float(diff["diff_drain"]) / 10
+
             df = pd.read_csv(CSV_PATH + title + '/' + diff['diff'] + '.csv')
             count = 0
             for item in df.values:
                 if item[1] > 0:
+                    note_list.append(item[1])
                     count_list.append(count)
                     count = 0
                 else:
                     count += 1
+            while len(note_list) % 16 != 0:
+                note_list.append(0)
+                count_list.append(count_max)
 
-    count_list = np.log(np.array(count_list) + 1)
-    count_min = np.min(count_list)
-    count_max = np.max(count_list)
-    count_norm = (count_list - count_min) / (count_max - count_min)
-    count_list = count_norm.tolist()
-    count_list.sort()
-    plt.plot(count_list)
-    plt.show()
+
+
+            break
     '''
     for song in song_list:
         title = song["title"]
